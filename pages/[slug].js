7965d9fa1681581cbe6f489be9dffa042688e818
readeral <alan@readeral.com>
2020-03-11 19:58:57 +1100

    new paradigm for fetching data

--- pages/[slug].js
+++ pages/[slug].js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import Header from '../components/header';
 import BlockContent from '@sanity/block-content-to-react';
-import sanity from '../lib/sanity';
+import {fetchQuery} from '../lib/sanity';
 import Form from '../components/form';
 import getVideoId from 'get-video-id';
 import Vimeo from '@u-wave/react-vimeo';
@@ -13,6 +13,7 @@ import AudioPlayer from 'react-audio-player'
 import MainImage from '../components/main-image';
 import Layout from '../components/layout';
 import SanityBlock from '../utils/sanity-block';
+import {pageQuery} from '../lib/queries';
 
 const Grid = styled.div`
   display: grid;
@@ -24,11 +25,13 @@ const Wrapper = styled.article`
 `;
 
 export default function SanityPage({mainData}) {
+  const {body} = mainData;
+
   return  (
     <Layout>
       <Grid>
         <Header heading={['Page1']} />
-        <SanityBlock blocks={mainData[0].content} />
+        <SanityBlock blocks={body} />
       </Grid>
     </Layout>
       
@@ -40,35 +43,10 @@ SanityPage.propTypes = {
 };
 
 SanityPage.getInitialProps = async ({query}) => {
-    const pageQuery = `
-      *[_type == "page" && slug.current match '${query.slug}']{
-        ...,
-          content[]{
-            ...,
-            _type == 'reference' => @-> {
-              ...,
-              blocks[] {
-                ...,
-                _type == 'reference' => @ ->
-              }
-            },
-            markDefs[] {
-              ...,
-              _type == 'internalLink' => {
-                  'slug': @.reference->slug.current
-              }
-            },
-          asset->
-          },
-          mainImage{
-            asset->
-          },
-          'id': _id,
-        'pathname': '/' + slug.current
-      }
-    `;
-    
-    const results = await sanity.fetch(pageQuery);
-    console.log(results)
-    return {mainData: results};
+    const results = await fetchQuery(`
+    {
+      "mainData": ${pageQuery(query.slug)}
+    }
+    `);
+    return results;
   };
