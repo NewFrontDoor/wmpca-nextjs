629cc9e227f20046ed2e84a3c81dd28910cb35ae
Christian De Kievit <cdekievit@gmail.com>
2026-08-23 20:37:29 +1000

    Cleaned up the build issues

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,11 @@
-// Next.config.ts
+import type { NextConfig } from "next";
 
-import withPlugins from "next-compose-plugins";
-import withImages from "next-images";
-import withFonts from "next-fonts";
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
 	env: {
 		GOOGLE_MAPS_API: process.env.GOOGLE_MAPS_API,
 		SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
 		SANITY_ACCESS_TOKEN: process.env.SANITY_ACCESS_TOKEN
-	},
-	webpack: (config, options) => {
-		// Modify the `config` here
-
-		return config;
 	}
 };
 
-module.exports = withPlugins([withFonts, [withImages, { ignoreTypes: ["svg"] }]], nextConfig);
+export default nextConfig;
